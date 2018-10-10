efd7ecbfe3766b0191d24bd077e4dbd4780dee10
Tejubhat <teju.rbhat@gmail.com>
2018-10-09 20:01:54 -0700

    pushing first commit to github

---
+++ script.js
@@ -0,0 +1,11 @@
+//saving found secret div to var found
+var found = document.getElementById("found");
+var starting_widt = window.innerWidth;
+
+
+
+if (found !=null) {
+console.log("hi");
+} else {
+  console.log(found);
+}
